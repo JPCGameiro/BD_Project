68d6a8361a864485df3b8b5bf7dfc164503d4ed6
pedro.abreu@ua.pt <pedro.abreu@ua.pt>
2021-06-14 12:35:48 +0100

    now added views file

--- SQL_scripts/views.sql
+++ SQL_scripts/views.sql
@@ -8,15 +8,27 @@ CREATE VIEW EM.V_OVERVIEW AS
 	FROM		EM.PROMOTOR INNER JOIN
 					  EM.EVENTO ON EM.PROMOTOR.numCC = EM.EVENTO.cc_promotor INNER JOIN
 					  EM.CONCERTO ON EM.EVENTO.id = EM.CONCERTO.id_evento INNER JOIN
-					  EM.BANDA ON EM.CONCERTO.id_banda = EM.BANDA.id
+					  EM.BANDA ON EM.CONCERTO.id_banda = EM.BANDA.id;
 
 GO;
 
 CREATE VIEW EM.V_CONCERTOS AS
-	SELECT			EM.CONCERTO.id, EM.EVENTO.nome, EM.CONCERTO.id_evento, EM.BANDA.nome AS banda, EM.CONCERTO.id_banda, EM.CONCERTO.datatimeini, EM.CONCERTO.duracao
-	FROM            EM.CONCERTO INNER JOIN
-                         EM.BANDA ON EM.CONCERTO.id_banda = EM.BANDA.id INNER JOIN
-                         EM.EVENTO ON EM.CONCERTO.id_evento = EM.EVENTO.id INNER JOIN
-                         EM.SOUNDCHECK ON EM.CONCERTO.id_soundcheck = EM.SOUNDCHECK.id
+	SELECT		EM.CONCERTO.id, EM.EVENTO.nome, EM.CONCERTO.id_evento, EM.BANDA.nome AS banda, EM.CONCERTO.id_banda, EM.CONCERTO.datatimeini, EM.CONCERTO.duracao
+	FROM        EM.CONCERTO INNER JOIN
+                        EM.BANDA ON EM.CONCERTO.id_banda = EM.BANDA.id INNER JOIN
+                        EM.EVENTO ON EM.CONCERTO.id_evento = EM.EVENTO.id INNER JOIN
+                        EM.SOUNDCHECK ON EM.CONCERTO.id_soundcheck = EM.SOUNDCHECK.id;
 
-select * from EM.V_CONCERTOS
+GO;
+
+CREATE VIEW EM.V_BANDAS AS
+	SELECT		id, nome, telefone, email, numElem, genero
+	FROM		EM.BANDA;
+
+GO;
+
+CREATE VIEW EM.V_MUSICOS AS
+	SELECT		EM.MUSICO.numCC, EM.PESSOA.nome, EM.MUSICO.nomeArst, EM.PESSOA.email, EM.PESSOA.sexo, EM.BANDA.nome AS banda
+	FROM		EM.MUSICO INNER JOIN
+						EM.PESSOA ON EM.MUSICO.numCC = EM.PESSOA.numCC INNER JOIN
+						EM.BANDA ON EM.MUSICO.id_banda = EM.BANDA.id;
