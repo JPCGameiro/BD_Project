6767d75387f48e3d0627ee27a550ff871f225881
JPCGameiro <joao.gameiro@ua.pt>
2021-06-16 00:32:05 +0100

    Update novos triggers

--- SQL_scripts/triggers_EVENTMANAGEMENT.sql
+++ SQL_scripts/triggers_EVENTMANAGEMENT.sql
@@ -41,12 +41,6 @@
 	INSERT INTO EM.EVENTO SELECT * FROM inserted;
  END
 
- SELECT * FROM EM.EVENTO;
- --Teste
- INSERT INTO EM.EVENTO VALUES(25, 'TEste', 4, '2018-06-03', '2018-06-05', 1234, 114444, '2017-12-01', 32032541)
-
-
-
 
 
 
@@ -100,6 +94,57 @@ END
 
 
 
+/* soundcheck tem de ser pelo menos três horas antes de um concerto (se não for o soundcheck é apagado)  */
+GO
+CREATE TRIGGER concert_soundcheck ON EM.CONCERTO
+AFTER INSERT 
+AS
+BEGIN
+	DECLARE @s_fim AS DATETIME;
+	DECLARE @concert_start AS DATETIME;
+	DECLARE @soundcheck_id AS VARCHAR(20);
+
+	SELECT @concert_start = datatimeini, @soundcheck_id = id_soundcheck FROM inserted;
+
+	SELECT @s_fim = DATEADD(SECOND, DATEPART(SECOND,duracao), DATEADD(MINUTE, DATEPART(MINUTE, duracao), DATEADD(hour, DATEPART(hour,duracao), datatimeini)))
+	FROM EM.SOUNDCHECK
+	WHERE id = @soundcheck_id;
+
+	SELECT @s_fim = DATEADD(HOUR, DATEPART(hour,'02:59:00'), @s_fim)
+	PRINT @s_fim
+	PRINT @concert_start
+
+	IF (@s_fim > @concert_start) 
+	BEGIN
+		DELETE FROM EM.SOUNDCHECK WHERE id = @soundcheck_id;
+		RAISERROR('ERROR: Soundcheck must be at least three hours before the concert', 16, 1);
+		RETURN;
+	END
+END
+
+
+
+
+/* Soundcheck não pode durar mais de 2 horas (se durar a sua duração é ajustada para duas horas) */
+GO
+CREATE TRIGGER soundcheck_duration ON EM.SOUNDCHECK
+AFTER INSERT 
+AS
+BEGIN
+	DECLARE @duracao AS TIME;
+	DECLARE @id AS VARCHAR(20);
+
+	SELECT @id=id, @duracao = duracao  FROM inserted;
+
+	IF (@duracao > CONVERT(TIME, '02:00:00')) 
+	BEGIN
+		PRINT 'Soundcheck duration must be less or equal  than 2 hours'
+		UPDATE EM.SOUNDCHECK
+		SET duracao = '02:00:00'
+		WHERE id = @id;
+	END
+END
+
 
 
 
@@ -116,6 +161,43 @@ BEGIN
 	DELETE FROM EM.SOUNDCHECK WHERE  id=@sc_id;
 END
 GO
+
+/* não podem existir dois soundchecks ao mesmo tempo */
+GO
+CREATE TRIGGER simultaneos_soundchecks ON EM.SOUNDCHECK
+INSTEAD OF INSERT 
+AS
+BEGIN
+	DECLARE @dataini AS DATE;
+	DECLARE @datafim AS DATE;
+	DECLARE @d_ini AS DATE;
+	DECLARE @d_fim AS DATE;
+
+	SELECT @dataini = inserted.datatimeini, @datafim = DATEADD(SECOND, DATEPART(SECOND,inserted.duracao), DATEADD(MINUTE, DATEPART(MINUTE, inserted.duracao), DATEADD(hour, DATEPART(hour, inserted.duracao), inserted.datatimeini))) FROM inserted;
+
+	DECLARE C CURSOR
+	FOR
+		SELECT datatimeini, DATEADD(SECOND, DATEPART(SECOND,duracao), DATEADD(MINUTE, DATEPART(MINUTE, duracao), DATEADD(hour, DATEPART(hour, duracao), datatimeini)))  AS fim 
+		FROM EM.SOUNDCHECK;
+
+	OPEN C;
+	FETCH C INTO @d_ini, @d_fim;
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		IF ((@dataini >= @d_ini AND @dataini <= @d_fim) OR (@datafim >= @d_ini AND @datafim <= @d_fim)) 
+		BEGIN
+			RAISERROR('ERROR: Soundcheck date overlaps other soundchecks', 16, 1);
+			CLOSE C;
+			DEALLOCATE C;
+			RETURN;
+		END
+		FETCH C INTO @d_ini, @d_fim;
+	END
+	CLOSE C;
+	DEALLOCATE C;
+
+	INSERT INTO EM.SOUNDCHECK SELECT * FROM inserted;
+END
 
 
 
